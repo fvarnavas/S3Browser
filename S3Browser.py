96a4f7f2aef6d561b3771b411c7458861e096679
fvarnavas <frank@varnavas.com>
2024-10-05 01:33:38 -0400

    test2

--- S3Browser.py
+++ S3Browser.py
@@ -1,15 +1,15 @@
 import boto3
 import os
-from http.server import SimpleHTTPRequestHandler, HTTPServer
-import urllib.parse
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import urlparse
 
 # Initialize the S3 client
 s3_client = boto3.client('s3')
 
 
-class S3BrowserHandler(SimpleHTTPRequestHandler):
+class S3BrowserHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        parsed_path = urllib.parse.urlparse(self.path)
+        parsed_path = urlparse.urlparse(self.path)
         path_parts = parsed_path.path.strip('/').split('/')
 
         if len(path_parts) == 1 and path_parts[0] == '':
@@ -29,7 +29,7 @@ class S3BrowserHandler(SimpleHTTPRequestHandler):
         self.wfile.write(b"<html><head><title>S3 Buckets</title></head><body>")
         self.wfile.write(b"<h1>S3 Buckets</h1><ul>")
         for bucket in buckets['Buckets']:
-            self.wfile.write(f"<li><a href='/bucket/{bucket['Name']}'>{bucket['Name']}</a></li>".encode())
+            self.wfile.write("<li><a href='/bucket/{0}'>{0}</a></li>".format(bucket['Name']))
         self.wfile.write(b"</ul></body></html>")
 
     def list_files(self, bucket_name):
@@ -38,16 +38,16 @@ class S3BrowserHandler(SimpleHTTPRequestHandler):
         self.send_header('Content-type', 'text/html')
         self.end_headers()
         self.wfile.write(b"<html><head><title>Files</title></head><body>")
-        self.wfile.write(f"<h1>Files in {bucket_name}</h1><ul>".encode())
+        self.wfile.write("<h1>Files in {0}</h1><ul>".format(bucket_name))
         for obj in objects.get('Contents', []):
-            self.wfile.write(f"<li><a href='/download/{bucket_name}/{obj['Key']}'>{obj['Key']}</a></li>".encode())
+            self.wfile.write("<li><a href='/download/{0}/{1}'>{1}</a></li>".format(bucket_name, obj['Key']))
         self.wfile.write(b"</ul></body></html>")
 
     def download_file(self, bucket_name, file_name):
         file_path = os.path.join('/tmp', file_name)
         s3_client.download_file(bucket_name, file_name, file_path)
         self.send_response(200)
-        self.send_header('Content-Disposition', f'attachment; filename="{file_name}"')
+        self.send_header('Content-Disposition', 'attachment; filename="{0}"'.format(file_name))
         self.send_header('Content-type', 'application/octet-stream')
         self.end_headers()
         with open(file_path, 'rb') as file:
